eae5915ea4dda163015b11f18a7069c8155a0816
Tharindu Athapaththu Hewage <48001737+Tharindu127@users.noreply.github.com>
2025-12-05 21:00:48 +0530

    Update avatar.js

--- src/app/components/3d/avatar.js
+++ src/app/components/3d/avatar.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useRef, useEffect, useState } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
