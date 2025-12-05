f8c12d330ad6a0b9926289db9f3b9d2c11b0317b
Tharindu Athapaththu Hewage <48001737+Tharindu127@users.noreply.github.com>
2025-12-05 21:07:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/tharinduathapaththu.portfolio.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/tharinduathapaththu.portfolio.io/' : '',
   images: {
     unoptimized: true,
   },
